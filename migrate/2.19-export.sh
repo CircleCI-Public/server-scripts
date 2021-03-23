48926f39286f5dbd36793a06cdefe4d88c67fce9
Ryan William O'Hara <ryan@ryanwohara.com>
2021-03-23 13:41:44 -0600

    Adding structure
    SERVER-518
    
    * Functions for everything
    * Preflight checks
    * Echo status updates during the process

--- migrate/2.19-export.sh
+++ migrate/2.19-export.sh
@@ -216,7 +216,8 @@ function compress() {
     mkdir -p circleci_export
 
     mv $PG_BU $MONGO_BU $KEY_BU $VAULT_BU circleci_export
-    tar cfz circleci_export_${DATE}.tar.gz circleci_export
+    rm -f circleci_export.tar.gz
+    tar cfz circleci_export.tar.gz circleci_export
 
     rm -rf circleci_export
 }
@@ -245,7 +246,7 @@ function circleci_database_export() {
 
 
     echo "CircleCI Server Export Complete."
-    echo "Your exported files can be found at $(pwd)/circleci_export_${DATE}.tar.gz"
+    echo "Your exported files can be found at $(pwd)/circleci_export.tar.gz"
 }
 
 circleci_database_export
