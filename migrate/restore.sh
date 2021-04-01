379f26655b6883d111b1f34ade5fe0b6d4a57603
Chris Johnson <chrislmjohnson@gmail.com>
2021-04-01 08:46:05 -0600

    Update migrate/restore.sh
    Co-authored-by: Nathan Smith <12156185+nsmith5@users.noreply.github.com>

--- migrate/restore.sh
+++ migrate/restore.sh
@@ -112,7 +112,7 @@ function import_vault() {
 function reinject_bottoken() {
     JOB_NAME=$(kubectl -n $NAMESPACE get jobs -o json | jq '.items[0].metadata.name' -r)
     echo "found job $JOB_NAME"
-    ### Re-run the job, but also remove some auto generated fields that canot be re-applied
+    ### Re-run the job, but also remove some auto generated fields that cannot be re-applied
     kubectl -n $NAMESPACE get job $JOB_NAME -o json | jq 'del(.spec.template.metadata.labels)' | jq 'del(.spec.selector)' | kubectl replace --force -f -
 }
 
