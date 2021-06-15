8c688f2c718bad11a743c94f1d49a1944804e895
Ryan W. O'Hara <ryanohara@circleci.com>
2021-06-15 13:47:23 -0600

    Implement db skip logic in 2.19 preflights

--- migrate/2.19-preflight.sh
+++ migrate/2.19-preflight.sh
@@ -17,34 +17,14 @@ function preflight_checks() {
         echo "Please shut down CircleCI from the replicated console at https://<YOUR_CIRCLE_URL>:8800 before running this script."
         exit 1
 
-    elif [ -n "$(docker ps | grep mongo | head -n1 )" ]
+    elif [[ ! "$SKIP_DATABASE_IMPORT" = "true" && -n "$(docker ps | grep mongo | head -n1 )" ]]
     then
         echo "Please shut down any other Mongo containers before running this script"
         exit 1
 
-    elif [ -n "$(docker ps | grep -v replicated | grep postgres | head -n1 )" ]
+    elif [[ ! "$SKIP_DATABASE_IMPORT" = "true" && -n "$(docker ps | grep -v replicated | grep postgres | head -n1 )" ]]
     then
         echo "Please shut down any other PostgreSQL containers before running this script"
-        exit 1
-    fi
-
-    if ! command -v jq >/dev/null 2>&1; then
-        echo ... installing jq
-        apt-get update
-        apt-get install -y jq
-    fi
-}
-
-function vault_preflight_checks() {
-
-    if [ $(id -u) -ne 0 ]
-    then
-        echo "Please run this script as root"
-        exit 1
-
-    elif [ -n "$(docker ps | grep circleci-frontend | head -n1 )" ]
-    then
-        echo "Please shut down CircleCI from the replicated console at https://<YOUR_CIRCLE_URL>:8800 before running this script."
         exit 1
     fi
 
