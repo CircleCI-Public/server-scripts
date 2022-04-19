599c50fceba958763c7fb4b4b93a1d2bca108937
Atul Singh <atul@circleci.com>
2022-04-19 10:52:51 -0400

    server-1712 | added sleep

--- migrate/3.0-vault.sh
+++ migrate/3.0-vault.sh
@@ -6,7 +6,8 @@ function import_vault() {
     echo "...importing Vault..."
 
     VAULT_POD="vault-0"
-    TEST_POD="z-test-pod"
+    TEST_POD="ztest"
+    TEST_POD_YAML="$DIR/ztest-pod.yml"
     VAULT_BU="${BACKUP_DIR}/circleci-vault"
 
     ### Seal
@@ -15,29 +16,54 @@ function import_vault() {
     # Scale down vault
     kubectl -n "$NAMESPACE" scale --replicas=0 sts/vault
 
+    # Waiting for Pod vault termination
+    while [[ $(kubectl get pods -l app=vault 2>/dev/null | grep -c vault) -gt 0 ]]
+    do
+        echo "Vault is terminating"
+        sleep 15
+    done
+
     # Creating a test pod
-    kubectl -n "$NAMESPACE" apply -f ./z-test-pod.yml
+    kubectl -n "$NAMESPACE" apply -f "$TEST_POD_YAML"
+
+    # Waiting for Pod ztest
+    while [[ ! $(kubectl get pods -l app=ztest 2>/dev/null | grep -c "1/1") -gt 0 ]]
+    do
+        echo "Waiting for ztest to scale up"
+        sleep 15
+    done
+
+    sleep 10 && kubectl get pods -l app=ztest
 
     # Check the content
-    kubectl -n "$NAMESPACE" exec "$TEST_POD" -- ls -l /vault/file
+    kubectl -n "$NAMESPACE" exec "$TEST_POD" -- ls -l /tmp/vault/ /tmp/vault/file
 
     # Delete the PVC content
-    kubectl -n "$NAMESPACE" exec "$TEST_POD" -- rm -rf /vault/file
+    kubectl -n "$NAMESPACE" exec "$TEST_POD" -- rm -rf /tmp/vault/file/*
 
     # Copy the vault backup into test pod
-    kubectl -n "$NAMESPACE" cp -v=2 -c vault "$VAULT_BU"/vault-backup.tar.gz "$TEST_POD":/tmp/vault-backup.tar.gz
+    kubectl -n "$NAMESPACE" cp -v=2 "$VAULT_BU"/vault-backup.tar.gz "$TEST_POD":/tmp/vault-backup.tar.gz
 
     # Extract the content
-    kubectl -n "$NAMESPACE" exec "$TEST_POD" -- tar -xvzf /tmp/vault-backup.tar.gz -C /vault/
+    kubectl -n "$NAMESPACE" exec "$TEST_POD" -- tar -xvzf /tmp/vault-backup.tar.gz -C /tmp/vault/
 
     # Check if content is extracted
-    kubectl -n "$NAMESPACE" exec "$TEST_POD" -- ls -l /vault/file
+    kubectl -n "$NAMESPACE" exec "$TEST_POD" -- ls -l /tmp/vault/ /tmp/vault/file
 
     # If Yes, delete the pod now
-    kubectl -n "$NAMESPACE" delete "$TEST_POD"
+    kubectl -n "$NAMESPACE" delete pod/"$TEST_POD"  
 
     # Bring up the Vault sts and wait to be up
-    kubectl -n "$NAMESPACE" scale --replicas=1 sts/vaults
+    kubectl -n "$NAMESPACE" scale --replicas=1 sts/vault
+
+    # Waiting for Pod vault
+    while [[ ! $(kubectl get pods -l app=vault 2>/dev/null | grep -c "2/2") -gt 0 ]]
+    do
+        echo "Waiting for Vault to scale up"
+        sleep 15
+    done 
+
+    sleep 10 && kubectl get pods -l app=vault
 
     ### Unseal
     kubectl -n "$NAMESPACE" exec "$VAULT_POD" -c vault -- sh -c "rm -rf /vault/file/sys/expire/id/auth/token/create/* && for k in \$(head -n 3 /vault/file/vault-init-out | sed \"s/^.*: //g\"); do vault operator unseal \"\$k\"; done; vault login \$(grep \"Root\" /vault/file/vault-init-out | sed \"s/^.*: //g\"); vault token create -period=\"768h\" > /vault/file/client-token; grep \"token \" /vault/file/client-token | sed \"s/^token\W*//g\" > /vault/__restricted/client-token"
