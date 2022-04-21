fde84ba21fdeb5db10c02b1c799e9e77dd11ee3f
Atul Singh <atul@circleci.com>
2022-04-21 06:08:39 -0400

    server-1712 | added namespace in kubectl command

--- migrate/3.0-vault.sh
+++ migrate/3.0-vault.sh
@@ -15,38 +15,38 @@ function import_vault() {
     ## Seal
     kubectl -n "$NAMESPACE" exec "$VAULT_POD" -c vault -- vault operator seal
 
-    # Scale down vault
+    # Scaling down Vault
     kubectl -n "$NAMESPACE" scale --replicas=0 sts/vault
 
-    # Waiting for Pod vault termination
-    while [[ $(kubectl get pods -l app=vault 2>/dev/null | grep -c vault) -gt 0 ]]
+    # Waiting for Vault termination
+    while [[ $(kubectl -n "$NAMESPACE" get pods -l app=vault 2>/dev/null | grep -c vault) -gt 0 ]]
     do
         echo "Vault is terminating"
         sleep 15
     done
 
     # Fetching the PVC info
-    VOL_SIZE=`kubectl get pvc/$VAULT_PVC -o=jsonpath="{.status.capacity.storage}"`
+    VOL_SIZE=$(kubectl -n "$NAMESPACE" get pvc/$VAULT_PVC -o=jsonpath="{.status.capacity.storage}")
     echo "Volume size of $VAULT_PVC is - $VOL_SIZE"
 
     # Deleting the PVC
-    kubectl delete pvc/$VAULT_PVC
+    kubectl -n "$NAMESPACE" delete pvc/$VAULT_PVC
 
     # Recreating the PVC
-    cat "$VAULT_PVC_YAML" | sed  "s/VOLUME_SIZE/$VOL_SIZE/" | kubectl -n "$NAMESPACE" apply -f -
-    kubectl get pvc/$VAULT_PVC
+    sed  "s/VOLUME_SIZE/$VOL_SIZE/" < "$VAULT_PVC_YAML" | kubectl -n "$NAMESPACE" apply -f -
+    kubectl -n "$NAMESPACE" get pvc/$VAULT_PVC
 
     # Creating a test pod
     kubectl -n "$NAMESPACE" apply -f "$TEST_POD_YAML"
 
     # Waiting for Pod ztest
-    while [[ ! $(kubectl get pods -l app=ztest 2>/dev/null | grep -c "1/1") -gt 0 ]]
+    while [[ ! $(kubectl -n "$NAMESPACE" get pods -l app=ztest 2>/dev/null | grep -c "1/1") -gt 0 ]]
     do
         echo "Waiting for ztest to scale up"
         sleep 15
     done
 
-    sleep 10 && kubectl get pods -l app=ztest
+    kubectl -n "$NAMESPACE" get pods -l app=ztest
 
     # Copy the vault backup into test pod
     kubectl -n "$NAMESPACE" cp -v=2 "$VAULT_BU"/vault-backup.tar.gz "$TEST_POD":/tmp/vault-backup.tar.gz
@@ -64,13 +64,13 @@ function import_vault() {
     kubectl -n "$NAMESPACE" scale --replicas=1 sts/vault
 
     # Waiting for Pod vault
-    while [[ ! $(kubectl get pods -l app=vault 2>/dev/null | grep -c "2/2") -gt 0 ]]
+    while [[ ! $(kubectl -n "$NAMESPACE" get pods -l app=vault 2>/dev/null | grep -c "2/2") -gt 0 ]]
     do
         echo "Waiting for Vault to scale up"
         sleep 15
     done 
 
-    sleep 10 && kubectl get pods -l app=vault
+    kubectl -n "$NAMESPACE" get pods -l app=vault
 
     ### Unseal
     kubectl -n "$NAMESPACE" exec "$VAULT_POD" -c vault -- sh -c "rm -rf /vault/file/sys/expire/id/auth/token/create/* && for k in \$(head -n 3 /vault/file/vault-init-out | sed \"s/^.*: //g\"); do vault operator unseal \"\$k\"; done; vault login \$(grep \"Root\" /vault/file/vault-init-out | sed \"s/^.*: //g\"); vault token create -period=\"768h\" > /vault/file/client-token; grep \"token \" /vault/file/client-token | sed \"s/^token\W*//g\" > /vault/__restricted/client-token"
