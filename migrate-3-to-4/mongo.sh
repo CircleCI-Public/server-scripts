1ee926436b61ecc8a4dde761bc0893c8798343fb
Ryan William O'Hara <ryanwohara@users.noreply.github.com>
2023-07-07 10:00:32 -0600

    Update migrate-3-to-4/mongo.sh
    Co-authored-by: Atul S <3430610+atulsingh0@users.noreply.github.com>

--- migrate-3-to-4/mongo.sh
+++ migrate-3-to-4/mongo.sh
@@ -5,7 +5,7 @@ function import_mongo() {
 
     MONGO_POD="mongodb-0"
     MONGODB_USERNAME="root"
-    MONGODB_PASSWORD=$(kubectl -n "$NAMESPACE" get secrets mongodb-credentials -o jsonpath="{.data.mongodb-root-password}" | base64 --decode)
+    MONGODB_PASSWORD=$(kubectl -n "$NAMESPACE" get secrets mongodb -o jsonpath="{.data.mongodb-root-password}" | base64 --decode)
 
     kubectl -n "$NAMESPACE" exec "$MONGO_POD" -- mkdir -p /bitnami/mongodb/tmp/backups/
     kubectl -n "$NAMESPACE" cp -v=2 "$MONGO_BU" "$MONGO_POD":/bitnami/mongodb/tmp/backups/
