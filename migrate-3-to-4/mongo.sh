c382319c644412ac4a6eb9e55b6d75cb6061924e
Pat Freeman <github@e2r4.com>
2023-07-07 15:17:48 -0500

    migrate-3-to-4: use mongo data volume for backup (#45)
    * migrate-3-to-4: use mongo data volume for backup
    
    * Update migrate-3-to-4/mongo.sh
    
    Co-authored-by: Atul S <3430610+atulsingh0@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Pat Freeman <pfreeman@enova.com>
    Co-authored-by: Ryan William O'Hara <ryanwohara@users.noreply.github.com>
    Co-authored-by: Atul S <3430610+atulsingh0@users.noreply.github.com>

--- migrate-3-to-4/mongo.sh
+++ migrate-3-to-4/mongo.sh
@@ -7,9 +7,9 @@ function import_mongo() {
     MONGODB_USERNAME="root"
     MONGODB_PASSWORD=$(kubectl -n "$NAMESPACE" get secrets mongodb -o jsonpath="{.data.mongodb-root-password}" | base64 --decode)
 
-    kubectl -n "$NAMESPACE" exec "$MONGO_POD" -- mkdir -p /tmp/backups/
-    kubectl -n "$NAMESPACE" cp -v=2 "$MONGO_BU" "$MONGO_POD":/tmp/backups/
+    kubectl -n "$NAMESPACE" exec "$MONGO_POD" -- mkdir -p /bitnami/mongodb/tmp/backups/
+    kubectl -n "$NAMESPACE" cp -v=2 "$MONGO_BU" "$MONGO_POD":/bitnami/mongodb/tmp/backups/
 
-    kubectl -n "$NAMESPACE" exec "$MONGO_POD" -- mongorestore --drop -u "$MONGODB_USERNAME" -p "$MONGODB_PASSWORD" --authenticationDatabase admin /tmp/backups/circle-mongo;
-    kubectl -n "$NAMESPACE" exec "$MONGO_POD" -- rm -rf /tmp/backups
+    kubectl -n "$NAMESPACE" exec "$MONGO_POD" -- mongorestore --drop -u "$MONGODB_USERNAME" -p "$MONGODB_PASSWORD" --authenticationDatabase admin /bitnami/mongodb/tmp/backups/circle-mongo
+    kubectl -n "$NAMESPACE" exec "$MONGO_POD" -- rm -rf /bitnami/mongodb/tmp
 }
